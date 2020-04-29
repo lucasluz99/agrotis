93fe382be92e2a9fa3208ff780a58b9232997be0
lucasluz99 <lucasluzthatsme@gmail.com>
2020-04-29 19:30:11 -0300

    Add Header of NewDoc page

--- src/pages/NewDoc/index.js
+++ src/pages/NewDoc/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { IoIosArrowBack as BackIcon } from 'react-icons/io';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import Wrapper from '../../components/Wrapper';
+import { addDoc } from '../../store/modules/docs/actions';
+import { Container, Header, Actions } from './styles';
 
 export default function NewDoc() {
-  return <div />;
+  const dispatch = useDispatch();
+  return (
+    <Wrapper>
+      <Container>
+        <Header>
+          <div>
+            <Link to="/">
+              <BackIcon size={30} color="#fff" />
+            </Link>
+            <p>Situação</p>
+          </div>
+          <Actions>
+            <Link to="/">Voltar</Link>
+            <button type="button" onClick={() => dispatch(addDoc())}>
+              Salvar
+            </button>
+          </Actions>
+        </Header>
+      </Container>
+    </Wrapper>
+  );
 }
--- src/pages/NewDoc/styles.js
+++ src/pages/NewDoc/styles.js
@@ -1,3 +1,54 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  margin-top: 20px;
+  height: 70vh;
+  background: #fff;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  padding: 10px 20px;
+  width: 100%;
+  background-color: #145e43;
+  justify-content: space-between;
+  align-items: center;
+  p {
+    font-size: 22px;
+    color: #fff;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    svg {
+      margin-right: 5px;
+    }
+  }
+`;
+
+export const Actions = styled.div`
+  display: flex;
+  width: 200px;
+  justify-content: space-between;
+  a,
+  button {
+    padding: 10px 15px;
+    transition: background 200ms linear;
+    &:hover {
+      background: ${darken(0.05, '#145e43')};
+    }
+  }
+  a {
+    font-size: 22px;
+    text-decoration: none;
+    color: #fff;
+  }
+  button {
+    font-size: 22px;
+    background: none;
+    border: 0;
+    color: #fff;
+  }
+`;
